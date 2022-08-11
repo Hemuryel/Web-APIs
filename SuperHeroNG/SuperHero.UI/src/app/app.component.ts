7ef3d05956fe95e76ee994752d6f50e79b946ce8
Hemuryel <hyl.silva@gmail.com>
2022-08-10 23:45:04 -0300

    Adicionado download .csv e .pdf em Angular com .NET API

--- SuperHeroNG/SuperHero.UI/src/app/app.component.ts
+++ SuperHeroNG/SuperHero.UI/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SuperHero } from './models/super-hero';
 import { SuperHeroService } from './services/super-hero.service';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
+import { jsPDF } from 'jspdf';
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +14,18 @@ export class AppComponent {
   title = 'SuperHero.UI';
   heroes: SuperHero[] = [];
   heroToEdit?: SuperHero;
+  @ViewChild('content', {static: false}) el!: ElementRef;
 
   constructor(private superHeroService: SuperHeroService) { }
 
   ngOnInit(): void {
+    this.getSuperHeroes();
+  }
+
+  getSuperHeroes(){
     this.superHeroService
-      .getSuperHeroes()
-      .subscribe((result: SuperHero[]) => (this.heroes = result));
+    .getSuperHeroes()
+    .subscribe((result: SuperHero[]) => (this.heroes = result));
   }
 
   updateHeroList(heroes: SuperHero[]) {
@@ -30,5 +38,30 @@ export class AppComponent {
 
   editHero(hero: SuperHero) {
     this.heroToEdit = hero;
+  }
+
+  downloadCsv(){
+    var options = { 
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      //showLabels: true, 
+      //showTitle: true,
+      //title: 'SuperHeroNG',
+      useBom: true,
+      headers: ["Name", "First Name", "Last Name", "Place"]
+    };
+
+    new ngxCsv(this.heroes, "relatorio_superheroes", options);
+  }
+
+  downloadPdf(){
+    let pdf = new jsPDF('portrait', 'pt', 'A4');
+
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("modelo.pdf");
+      }
+    })
   }
 }
